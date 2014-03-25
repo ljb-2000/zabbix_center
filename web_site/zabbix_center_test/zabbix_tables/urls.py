e93a1c52977aa9262db6cf4207e3efcd61018bcc
stefanmonkey <stefan_bo@163.com>
2014-03-25 10:43:40 +0800

    switch development branch first commit

--- web_site/zabbix_center_test/zabbix_tables/urls.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-
-from zabbix_tables.views import *
-
-urlpatterns = patterns('',
-	url(r'^$', TableView.as_view(), name='home'), 
-)
