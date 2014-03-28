9afaaee42b2631ae3d789f5ed51ceedeb9773689
stefanmonkey <stefan_bo@163.com>
2014-03-28 17:07:28 +0800

    20140328

---
+++ web_site/zabbix_center/spider/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, url
+
+from spider import views
+
+urlpatterns = patterns('',
+	# ex: /polls/
+	url(r'^$', views.spider_index, name='spider_index'),
+	# ex: /polls/5/
+	#url(r'^hostgroup/(?P<group_id>\d+)/$', views.grouphost_detail, name='grouphost_detail'),
+    #url(r'^tests', views.tests, name='tests'),
+    #url(r'^hostgraph/(?P<host_id>\w+)/$', views.hostgraph_detail, name='hostgraph_detail'),
+    #url(r'^last_issue/$', views.issue_detail, name='issue_detail'),
+    # ex: /polls/5/results/
+    #url(r'^(?P<group_id>\d+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    #url(r'^(?P<group_id>\d+)/vote/$', views.vote, name='vote'),
+)
--- web_site/zabbix_center/spider/views.py
+++ web_site/zabbix_center/spider/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 # Create your views here.
+#from screens.models import Groups, HostsGroups, model_tests
+from api_process.process import *
+zabbix_url = process_base().zabbix_url
+
+def spider_index(request):
+	context = {'zabbix_url': zabbix_url}
+	return render(request, 'spider/index.html', context)
